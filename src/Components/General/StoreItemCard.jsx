401d8e9a9fb052e11fbb1710eaddaa5dc90e6606
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-08-05 12:03:54 +0100

    .

---
+++ src/Components/General/StoreItemCard.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect,useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faEye, faStar } from '@fortawesome/free-solid-svg-icons';
+
+
+export const StoreItemCard = ({ imgSrc, title, originalPrice, discountedPrice, stars, reviewCount }) => {
+    const [isFavorite, setIsFavorite] = useState(false);
+  
+    const handleFavoriteClick = () => {
+      setIsFavorite(!isFavorite);
+    };
+  
+    return (
+      <div className="w-80 bg-gray-100 relative">
+        <div className="overflow-hidden flex items-center justify-center">
+          <img src={imgSrc} alt="Item" className="h-52 w-full py-10 object-contain" />
+          <div className="absolute top-2 left-2 bg-red-600 text-white px-3 py-1 rounded-lg"> -35% </div>
+        </div>
+        <div className="mt-4 bg-white">
+          <p className="text-black font-semibold">{title}</p>
+          <div className="flex items-center mt-2">
+            <p className="text-gray-500 line-through mr-2">${originalPrice}</p>
+            <p className="text-red-500 font-semibold">${discountedPrice}</p>
+          </div>
+          <div className="flex items-center mt-2">
+            <div className="flex items-center">
+              {[...Array(5)].map((_, index) => (
+                <FontAwesomeIcon
+                  key={index}
+                  icon={faStar}
+                  className={`h-5 w-5 ${
+                    index < stars ? 'text-yellow-400' : 'text-gray-300'
+                  }`}
+                />
+              ))}
+            </div>
+            <p className="ml-2">({reviewCount})</p>
+          </div>
+        </div>
+  
+        {/* Positioned at the top right */}
+        <div className="absolute top-0 right-2 flex flex-col">
+          <div className="flex items-center">
+            <FontAwesomeIcon
+              icon={faHeart}
+              className={`h-6 w-6 bg-white rounded-full p-1 my-1 cursor-pointer ${
+                isFavorite ? 'text-red-500' : 'text-gray-300'
+              }`}
+              onClick={handleFavoriteClick}
+            />
+          </div>
+          <div className="flex items-center">
+            <FontAwesomeIcon icon={faEye} className="h-6 w-6 bg-white rounded-full p-1 my-1 text-gray-400" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
--- src/Components/Home/Contents.jsx
+++ src/Components/Home/Contents.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect,useRef } from 'react';
+import { StoreItemCard } from '../General/StoreItemCard';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faEye, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Contents = () => {
   const items = [
@@ -64,11 +67,14 @@ export const Contents = () => {
   ];
   
   return (
-    <div className='border-8 border-red-500 h-auto w-10/12 my-10 flex flex-col'>
+    <div className='border border-red-500 h-auto w-full my-10 flex flex-col items-end'>
       <Todays items={items}></Todays>
+      <Categories></Categories>
     </div>
   )
 }
+
+// todays section code starts
 
 export const CountdownTimer = ({ targetDate }) => {
   const calculateTimeLeft = () => {
@@ -100,7 +106,7 @@ export const CountdownTimer = ({ targetDate }) => {
   return (
     <div className="w-fit h-auto flex flex-row justify-evenly">
       {Object.entries(timeLeft).map(([unit, value], index, arr) => (
-        <div key={unit} className="text-center py-4 px-1">
+        <div key={unit} className="text-center px-1">
           <p className="text-sm text-gray-500 uppercase">{unit}</p>
           <p className="text-4xl font-bold">
             {value.toString().padStart(2, '0')}
@@ -111,114 +117,169 @@ export const CountdownTimer = ({ targetDate }) => {
     </div>
   );
 };
-
-
 export const Todays =({items}) => {
   const targetDate = '2023-12-31T00:00:00'; // Set your target date here
 
   return (
-    <div className='border border-black h-auto w-full my-10 flex flex-col'>
-        <div className='w-full h-11 mx-10  flex flex-row items-center'> <div className='h-full w-6 bg-red-600 rounded-lg'></div> <p className='text-red-600 text-lg font-semibold mx-3'>Today's</p> </div>
-        <div className='md:w-1/2 h-auto mx-10 flex flex-row items-center justify-between'> <h1 className='text-3xl font-bold'>Flash Sales</h1> <CountdownTimer targetDate={targetDate} /> </div>
-        <div className='my-8 mx-2 overflow-hidden'> <LRSlider items={items} visibleItems={3}/> </div>
+    <div className='h-auto w-11/12 my-10 flex flex-col'>
+        <div className='w-full h-11 flex flex-row items-center'> <div className='h-full w-6 bg-red-600 rounded-lg'></div> <p className='text-red-600 text-lg font-semibold mx-3'>Today's</p> </div>
+        <div className='w-auto h-auto flex flex-row items-end my-2'> <h1 className='text-3xl font-bold md:mr-9'>Flash Sales</h1> <CountdownTimer targetDate={targetDate} /> </div>
+        <div className='my-8 w-full flex items-center justify-center'> <StoreItemSlider items={items} /> </div>
         <div className='w-full h-auto flex items-center justify-center'> <button className='bg-red-600 px-4 py-2 text-white'>View All Products</button> </div>
     </div>
   )
 }
 
-
-export const StoreItemCard = ({ imgSrc, title, originalPrice, discountedPrice, stars, reviewCount }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
-
-  const handleFavoriteClick = () => {
-    setIsFavorite(!isFavorite);
-  };
-
-  return (
-    <div className="w-80 bg-gray-100 relative">
-      <div className="overflow-hidden flex items-center justify-center">
-        <img src={imgSrc} alt="Item" className="h-52 w-full py-6 object-contain" />
-        <div className="absolute top-2 left-2 bg-red-600 text-black px-2 py-1 rounded-lg"> -35% </div>
-      </div>
-      <div className="mt-4 bg-white">
-        <p className="text-black font-semibold">{title}</p>
-        <div className="flex items-center mt-2">
-          <p className="text-gray-500 line-through mr-2">${originalPrice}</p>
-          <p className="text-red-500 font-semibold">${discountedPrice}</p>
-        </div>
-        <div className="flex items-center mt-2">
-          <div className="flex items-center">
-            {[...Array(5)].map((_, index) => (
-              <FontAwesomeIcon
-                key={index}
-                icon={faStar}
-                className={`h-5 w-5 ${
-                  index < stars ? 'text-yellow-400' : 'text-gray-300'
-                }`}
-              />
-            ))}
-          </div>
-          <p className="ml-2">({reviewCount})</p>
-        </div>
-      </div>
-
-      {/* Positioned at the top right */}
-      <div className="absolute top-2 right-2 flex flex-col">
-        <div className="flex items-center">
-          <FontAwesomeIcon
-            icon={faHeart}
-            className={`h-6 w-6 bg-white rounded-full p-1 my-1 cursor-pointer ${
-              isFavorite ? 'text-red-500' : 'text-gray-300'
-            }`}
-            onClick={handleFavoriteClick}
-          />
-        </div>
-        <div className="flex items-center">
-          <FontAwesomeIcon icon={faEye} className="h-6 w-6 bg-white rounded-full p-1 my-1 text-gray-400" />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-
-export const LRSlider = ({ items, visibleItems }) => {
-  const sliderRef = useRef(null);
+export const StoreItemSlider = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const itemsPerPage = 3;
 
-  const totalSlides = items.length;
-  const totalVisibleSlides = Math.ceil(totalSlides / visibleItems);
+  const numSlides = Math.ceil(items.length / itemsPerPage);
+  const showPrevButton = currentIndex > 0;
+  const showNextButton = currentIndex < numSlides - 1;
 
   const nextSlide = () => {
-    const nextIndex = (currentIndex + 1) % totalVisibleSlides;
-    setCurrentIndex(nextIndex);
-    sliderRef.current.style.transform = `translateX(-${(100 / totalVisibleSlides) * nextIndex}%)`;
+    setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, numSlides - 1));
   };
 
   const prevSlide = () => {
-    const prevIndex = (currentIndex - 1 + totalVisibleSlides) % totalVisibleSlides;
-    setCurrentIndex(prevIndex);
-    sliderRef.current.style.transform = `translateX(-${(100 / totalVisibleSlides) * prevIndex}%)`;
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
   };
 
+  const sliderTransform = `translateX(-${currentIndex * (100 / numSlides)}%)`;
+
   return (
-    <div className="relative">
-      <div ref={sliderRef} className="flex overflow-hidden" style={{ scrollBehavior: 'smooth', transition: 'transform 0.5s ease' }}>
+    <div className="relative w-full overflow-hidden">
+      <div className="flex transition-transform ease-in-out duration-300" style={{ transform: sliderTransform }}>
         {items.map((item, index) => (
-          <div key={index} className={`w-${100 / visibleItems} px-2 mx-4`}>
-            <StoreItemCard
-              imgSrc={item.imgSrc}
-              title={item.title}
-              originalPrice={item.originalPrice}
-              discountedPrice={item.discountedPrice}
-              stars={item.stars}
-              reviewCount={item.reviewCount}
-            />
+          <div key={index} className="w-80 mr-4">
+            <StoreItemCard {...item} />
           </div>
         ))}
       </div>
-      <button className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-red-600 p-2 rounded-full" onClick={prevSlide}>  &lt; </button>
-      <button className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-red-600 p-2 rounded-full" onClick={nextSlide}> &gt; </button>
+      {showPrevButton && (
+        <button
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 px-3 py-2 rounded-lg"
+          onClick={prevSlide}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} className="text-gray-500" />
+        </button>
+      )}
+      {showNextButton && (
+        <button
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 px-3 py-2 rounded-lg"
+          onClick={nextSlide}
+        >
+          <FontAwesomeIcon icon={faChevronRight} className="text-gray-500" />
+        </button>
+      )}
     </div>
   );
 };
+
+// todays section code ends
+
+// categories section code starts
+const CategoriesSlider = ({ CategoriesItems }) => {
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const itemsPerPage = 3;
+
+  const numSlides = Math.ceil(CategoriesItems.length / itemsPerPage);
+  const showPrevButton = currentIndex > 0;
+  const showNextButton = currentIndex < numSlides - 1;
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, numSlides - 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+
+  const sliderTransform = `translateX(-${currentIndex * (100 / numSlides)}%)`;
+
+  return (
+    <div className="relative w-full overflow-hidden">
+      <div className="flex transition-transform ease-in-out duration-300" style={{ transform: sliderTransform }}>
+        {CategoriesItems.map((item, index) => (
+          <div key={index} className="w-80 mr-4">
+            <CategoriesSliderItems {...item} />
+          </div>
+        ))}
+      </div>
+      {showPrevButton && (
+        <button
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 px-3 py-2 rounded-lg"
+          onClick={prevSlide}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} className="text-gray-500" />
+        </button>
+      )}
+      {showNextButton && (
+        <button
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 px-3 py-2 rounded-lg"
+          onClick={nextSlide}
+        >
+          <FontAwesomeIcon icon={faChevronRight} className="text-gray-500" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+const CategoriesSliderItems = ({ imgSrc, title }) => {
+  return (
+    <div className="border p-6 flex flex-col items-center justify-center">
+      <img className="object-fit h-20 w-20" src={imgSrc} alt={title} />
+      <p className="text-black"> {title} </p>
+    </div>
+  );
+};
+
+export const Categories = () => {
+  const CategoriesItems = [
+    {
+      imgSrc: './img/Category-CellPhone.png',
+      title: 'Phones',
+    },
+    {
+      imgSrc: './img/Category-Computer.png',
+      title: 'Computers',
+    },
+    {
+      imgSrc: './img/Category-Smartwatch.png',
+      title: 'SmartWatch',
+    },
+    {
+      imgSrc: './img/Category-Camera.png',
+      title: 'Camera',
+    },
+    {
+      imgSrc: './img/Category-Headphone.png',
+      title: 'HeadPhones',
+    },
+    {
+      imgSrc: './img/Category-CellPhone.png',
+      title: 'CellPhone',
+    },
+    
+  ];
+  
+  return (
+    <div className="h-auto w-11/12 my-10 flex flex-col">
+      <div className="w-11/2 h-11 flex flex-row items-center">
+        <div className="h-full w-6 bg-red-600 rounded-lg"></div>
+        <p className="text-red-600 text-lg font-semibold mx-3"> Categories </p>
+      </div>
+      <div className="w-auto h-auto flex flex-row items-end my-2">
+        <h1 className="text-3xl font-bold md:mr-9"> Browse By Category </h1>
+      </div>
+      <div className="my-8 w-11/12 h-full">
+        <CategoriesSlider CategoriesItems={CategoriesItems} /> 
+      </div>
+    </div>
+  );
+};
+
+// categories section code Ends
