0837ab8d70f96d24ed957e7a834aa0b83bbecdc6
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-08-08 15:06:38 +0100

    .

--- src/Components/Home/Contents.jsx
+++ src/Components/Home/Contents.jsx
@@ -61,29 +61,36 @@ const items = [
     stars: 4,
     reviewCount: 80,
   },
-  
+  {
+    imgSrc: './img/p1.png',
+    title: 'Body Louvre',
+    originalPrice: 50,
+    discountedPrice: 32,
+    stars: 4,
+    reviewCount: 80,
+  },
 ];
 
 export const Contents = () => {
   
   return (
-    <div className='h-auto w-full my-10 flex flex-col items-end'>
+    <div className='h-auto w-full my-10 flex flex-col items-end border border-black'>
       <Todays items={items}></Todays>
       <Categories></Categories>
       <ThisMonth></ThisMonth>
-      <div className='p-10 w-11/12 mx-auto flex flex-row bg-black'>
-          <div className='w-1/2 bg-green flex flex-col items-start justify-evenly'>
-            <p className='text-green-700 text-xl font-bold'> Categories </p>
-            <p className='text-6xl font-semibold text-white md:w-4/5'>Enhance Your Music Experience</p>
-            <div className='flex flex-row'> 
-              <div className='flex flex-col bg-white items-center justify-center rounded-full h-20 w-20 p-2 m-2'> <p className='font-bold text-xl'>23</p> <p className='font-semibold'>Hours</p> </div> 
-              <div className='flex flex-col bg-white items-center justify-center rounded-full h-20 w-20 p-2 m-2'> <p className='font-bold text-xl'>05</p> <p className='font-semibold'>Days</p> </div> 
-              <div className='flex flex-col bg-white items-center justify-center rounded-full h-20 w-20 p-2 m-2'> <p className='font-bold text-xl'>59</p> <p className='font-semibold'>Minutes</p> </div> 
-              <div className='flex flex-col bg-white items-center justify-center rounded-full h-20 w-20 p-2 m-2'> <p className='font-bold text-xl'>35</p> <p className='font-semibold'>Seconds</p> </div> 
+      <div className='p-10 w-10/12 mx-auto flex flex-col items-center justify-center md:flex-row bg-black'>
+          <div className='md:w-1/2 w-full bg-green flex flex-col md:items-start items-center md:text-left text-center justify-evenly'>
+            <p className='text-green-700 my-5 text-xl font-bold'> Categories </p>
+            <p className='lg:text-6xl text-3xl my-5 font-semibold text-white md:w-4/5'>Enhance Your Music Experience</p>
+            <div className='flex flex-row my-5'> 
+              <div className='flex flex-col bg-white items-center justify-center rounded-full lg:h-20 lg:w-20 w-16 p-2 m-2'> <p className='font-bold lg:text-xl '>23</p> <p className='font-semibold text-xs lg:text-lg'>Hours</p> </div> 
+              <div className='flex flex-col bg-white items-center justify-center rounded-full lg:h-20 lg:w-20 w-16 p-2 m-2'> <p className='font-bold lg:text-xl '>05</p> <p className='font-semibold text-xs lg:text-lg'>Days</p> </div> 
+              <div className='flex flex-col bg-white items-center justify-center rounded-full lg:h-20 lg:w-20 w-16 p-2 m-2'> <p className='font-bold lg:text-xl '>59</p> <p className='font-semibold text-xs lg:text-lg'>Minutes</p> </div> 
+              <div className='flex flex-col bg-white items-center justify-center rounded-full lg:h-20 lg:w-20 w-16 p-2 m-2'> <p className='font-bold lg:text-xl '>35</p> <p className='font-semibold text-xs lg:text-lg'>Seconds</p> </div> 
             </div>
-            <button className='bg-green-500 rounded text-white font-semibold py-3 px-8 my-x'>Buy Now!</button>
+            <button className='bg-green-500 my-5 rounded text-white font-semibold py-3 px-8 my-x'>Buy Now!</button>
           </div>
-          <div className='w-1/2 flex items-center justify-center'> <img src='./img/JBL_BOOMBOX_2_HERO_020_x1 (1) 1.png' className='object-fit h-[370px] w-auto h-auto py-10' /> </div>
+          <div className='w-1/2 flex items-center justify-center'> <img src='./img/JBL_BOOMBOX_2_HERO_020_x1 (1) 1.png' className='object-fit h-[370px] w-auto py-10' /> </div>
       </div>
       <ExploreProducts></ExploreProducts>
       <NewArrival></NewArrival>
@@ -139,10 +146,12 @@ export const Todays =({items}) => {
   const targetDate = '2023-12-31T00:00:00'; // Set your target date here
 
   return (
-    <div className='h-auto w-11/12 my-10 flex flex-col'>
+    <div className='h-auto w-11/12 my-10 flex flex-col  border border-black'>
         <div className='w-full h-11 flex flex-row items-center'> <div className='h-full w-6 bg-red-600 rounded-lg'></div> <p className='text-red-600 text-lg font-semibold mx-3'>Today's</p> </div>
         <div className='w-auto h-auto flex flex-row items-end my-2'> <h1 className='text-3xl font-bold md:mr-9'>Flash Sales</h1> <CountdownTimer targetDate={targetDate} /> </div>
-        <div className='my-8 w-full flex items-center justify-center'> <StoreItemSlider items={items} /> </div>
+        <div className='my-8 w-full flex items-center justify-center'>
+           {/* <StoreItemSlider items={items} />  */}
+           </div>
         <div className='w-full h-auto flex items-center justify-center'> <button className='bg-red-600 px-4 py-2 text-white'>View All Products</button> </div>
     </div>
   )
@@ -198,62 +207,6 @@ export const StoreItemSlider = ({ items }) => {
 // todays section code ends
 
 // categories section code starts
-const CategoriesSlider = ({ CategoriesItems }) => {
-  
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const itemsPerPage = 3;
-
-  const numSlides = Math.ceil(CategoriesItems.length / itemsPerPage);
-  const showPrevButton = currentIndex > 0;
-  const showNextButton = currentIndex < numSlides - 1;
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, numSlides - 1));
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
-  };
-
-  const sliderTransform = `translateX(-${currentIndex * (100 / numSlides)}%)`;
-
-  return (
-    <div className="relative w-full overflow-hidden">
-      <div className="flex transition-transform ease-in-out duration-300" style={{ transform: sliderTransform }}>
-        {CategoriesItems.map((item, index) => (
-          <div key={index} className="w-80 mr-4">
-            <CategoriesSliderItems {...item} />
-          </div>
-        ))}
-      </div>
-      {showPrevButton && (
-        <button
-          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-gray-200 px-3 py-2 rounded-lg"
-          onClick={prevSlide}
-        >
-          <FontAwesomeIcon icon={faChevronLeft} className="text-gray-500" />
-        </button>
-      )}
-      {showNextButton && (
-        <button
-          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-200 px-3 py-2 rounded-lg"
-          onClick={nextSlide}
-        >
-          <FontAwesomeIcon icon={faChevronRight} className="text-gray-500" />
-        </button>
-      )}
-    </div>
-  );
-};
-
-const CategoriesSliderItems = ({ imgSrc, title }) => {
-  return (
-    <div className="border border-gray-700 p-6 flex flex-col items-center justify-center">
-      <img className="object-fit h-20 w-20" src={imgSrc} alt={title} />
-      <p className="text-black"> {title} </p>
-    </div>
-  );
-};
 
 export const Categories = () => {
   const CategoriesItems = [
@@ -285,7 +238,7 @@ export const Categories = () => {
   ];
   
   return (
-    <div className="h-auto w-full my-10 flex flex-col items-center justify-center">
+    <div className="h-auto w-full my-10 flex flex-col items-center justify-center  border border-black">
       <div className='w-10/12'>
         <div className="w-full h-11 flex flex-row items-center">
           <div className="h-full w-6 bg-red-600 rounded-lg"></div>
@@ -294,14 +247,26 @@ export const Categories = () => {
         <div className="w-auto h-auto flex flex-row items-end my-2">
           <h1 className="text-3xl font-bold md:mr-9"> Browse By Category </h1>
         </div>
-        <div className="my-8 h-full">
-          <CategoriesSlider CategoriesItems={CategoriesItems} /> 
+        <div className="my-8 h-full flex md:flex-row md:flex-nowrap flex-wrap items-center justify-center">
+        {CategoriesItems.map((item, index) => (
+          <div key={index} className="w-80 mr-4">
+            <CategoriesSliderItems {...item} />
+          </div>
+        ))}
         </div>
       </div>
     </div>
   );
 };
 
+const CategoriesSliderItems = ({ imgSrc, title }) => {
+  return (
+    <div className="border border-gray-700 m-2 lg:p-6 p-2 flex flex-col items-center justify-center">
+      <img className="object-fit lg:h-16 lg:w-16 md:w-12 md:h-12 " src={imgSrc} alt={title} />
+      <p className="text-black"> {title} </p>
+    </div>
+  );
+};
 // categories section code Ends
 
 // This Month section code Starts
@@ -371,7 +336,7 @@ export const ThisMonth = () => {
   const itemsToShow = items.slice(0, 4);
 
   return (
-    <div className='h-auto w-full my-10 items-center justify-center flex flex-col border'>
+    <div className='h-auto w-full my-10 items-center justify-center hidden md:flex flex-col'>
       <div className='w-10/12'>
         <div className='w-full h-11 flex flex-row items-center'>
           <div className='h-full w-6 bg-red-600 rounded-lg'></div>
@@ -381,9 +346,9 @@ export const ThisMonth = () => {
           <h1 className='text-3xl font-bold md:mr-9'>Best Selling Products</h1>
           <button className='bg-red-600 px-4 py-2 rounded-lg text-white'>View All</button>
         </div>
-        <div className='my-8 ml-10 w-auto flex items-center justify-center'>
+        <div className='my-8 md:flex'>
           {itemsToShow.map((item, index) => (
-            <div key={index} className='w-80 mr-2'>
+            <div key={index} className='w-80 md:mr-2'>
               <StoreItemCard {...item} />
             </div>
           ))}
@@ -400,7 +365,7 @@ export const ThisMonth = () => {
 export const ExploreProducts = () => {
   
   return (
-    <div className="h-auto w-full my-10 flex flex-col items-center justify-center">
+    <div className="h-auto w-full my-10 flex flex-col items-center justify-center ">
     <div className='w-10/12'>
       <div className="w-full h-11 flex flex-row items-center">
         <div className="h-full w-6 bg-red-600 rounded-lg"></div>
@@ -420,7 +385,7 @@ export const ExploreProducts = () => {
 export const ExploreProductsGrid = ({ items }) => {
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="border border-black w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {items.map((item) => (
         <div key={item.id} className="p-4">
           <StoreItemCard {...item} />
@@ -493,9 +458,9 @@ export const NewArrival = () => {
 export const Attributes = () => {
   return (
     <div className="flex justify-center w-full items-center">
-      <div className='w-10/12 flex flex-row justify-between'>
+      <div className='w-10/12 flex md:flex-row flex-col items-center justify-between'>
 
-      <div className="w-1/3 p-4 rounded-lg m-2 flex flex-col items-center justify-center text-center">
+      <div className="md:w-1/3 w-5/6 p-4 rounded-lg m-2 flex flex-col items-center justify-center text-center">
         <div className="relative w-16 h-16 rounded-full bg-black border-4 border-gray-500 flex items-center justify-center">
           <img src="./img/icon-delivery.png" alt="Happy Icon" className="w-10 h-10" />
         </div>
@@ -503,7 +468,7 @@ export const Attributes = () => {
         <p className="text-sm text-gray-600">Free delivery for all orders over $140</p>
       </div>
 
-      <div className="w-1/3 p-4 rounded-lg mx-2 flex flex-col items-center justify-center text-center">
+      <div className="md:w-1/3 w-5/6  p-4 rounded-lg mx-2 flex flex-col items-center justify-center text-center">
         <div className="relative w-16 h-16 rounded-full bg-black border-4 border-gray-500 flex items-center justify-center">
           <img src="./img/Icon-Customer service.png" alt="Happy Icon" className="w-10 h-10" />
         </div>
@@ -511,7 +476,7 @@ export const Attributes = () => {
         <p className="text-sm text-gray-600">Friendly 24/7 customer support</p>
       </div>
 
-      <div className="w-1/3 p-4 rounded-lg mx-2 flex flex-col items-center justify-center text-center">
+      <div className="md:w-1/3 w-5/6  p-4 rounded-lg mx-2 flex flex-col items-center justify-center text-center">
         <div className="relative w-16 h-16 rounded-full bg-black border-4 border-gray-500 flex items-center justify-center">
           <img src="./img/Icon-secure.png" alt="Happy Icon" className="w-10 h-10" />
         </div>
