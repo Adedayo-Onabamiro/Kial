a2eef83cd62b0e317d27c4b270c6950e0fcc65a3
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-08-09 08:21:25 +0100

    .

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -8,6 +8,7 @@ export const SignUp = () => {
         <div className='md:h-96 my-28 h-auto w-full flex flex-col md:flex-row items-center justify-between'>
             <div className='w-11/12  md:w-[48%] h-full bg-[#CBE4E8] flex items-center justify-center'> <img className='md:h-64 h-12 w-auto object-contain' src='./img/trolley.png' /> </div>
             <div className='w-11/12  md:w-[48%] h-full'>
+            {/* Create Account Form */}
             <div class="flex flex-col md:items-start items-center justify-center h-auto">
                 <h1 class="md:text-4xl text-xl text-center font-bold mb-2">Create an Account</h1>
                 <p class="text-sm text-gray-600 mb-4 text-center">Enter your details below</p>
@@ -30,9 +31,32 @@ export const SignUp = () => {
                 
                 <p class="text-sm"> Already have an account? <a href="#" class="underline">Login</a> </p>
             </div>
+            {/* Create Account Form */}
 
-             </div>
+            {/* Login Form */}
+            <div class="flex flex-col md:items-start items-center justify-center h-auto">
+                <h1 class="md:text-4xl text-xl text-center font-bold mb-4">Log in to Exclusive</h1>
+                <p class="text-sm text-gray-600 mb-4 text-center">Enter your details below</p>
+                
+                <div class="md:w-10/12 w-full mb-4">
+                    <input type="text" placeholder="Email or Phone Number" class="w-full border-b border-gray-300 focus:border-gray-500 outline-none px-2 py-1" />
+                </div>
+                
+                
+                <div class="md:w-10/12 w-full mb-4">
+                    <input type="password" placeholder="Password" class="w-full border-b border-gray-300 focus:border-gray-500 outline-none px-2 py-1" />
+                </div>
+                
+                <div className='flex md:w-10/12 w-full md:flex-row flex-col justify-between items-center'>
+                <button class="md:w-4/12 w-full bg-red-500 text-white py-2 rounded-md mb-2">Login</button>              
+                <p class="text-sm text-red-500"> <a href="#" class="underline">Forgot Password?</a> </p>
+                </div> 
+
+            </div>
+            {/* Login Form */}
+            </div>
         </div>
     </>
   )
 }
+
