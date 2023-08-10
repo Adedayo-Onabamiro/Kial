0c04a334a67cde73663e0a79730dd6cfa02ae0bc
Adedayo-Onabamiro <dayoonabamiro@gmail.com>
2023-08-10 18:34:57 +0100

    .

---
+++ src/ProductContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+const ProductContext = createContext();
+
+function ProductProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
+
+export { ProductContext, ProductProvider };
